2078f1f7b687bfa64c44157af32c182a0930aa7f
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-06-07 17:54:45 +0900

    docs(polyfill): add description

--- polyfill.ts
+++ polyfill.ts
@@ -1,7 +1,25 @@
 // Copyright 2023-latest Tomoki Miyauchi. All rights reserved. MIT license.
 // This module is browser compatible.
-
 // deno-lint-ignore-file ban-types no-explicit-any
+
+/**
+ * ## Polyfill
+ *
+ * Polyfill affects the global object. You must be very careful when using it.
+ *
+ * ```ts
+ * import "https://deno.land/x/memoization@$VERSION/polyfill.ts";
+ *
+ * const fib = ((num: number): number => {
+ *   if (num < 2) return num;
+ *
+ *   return fib(num - 1) + fib(num - 2);
+ * }).memo();
+ *
+ * fib(1000);
+ * ```
+ */
+
 import { type MapLike, memo } from "./memo.ts";
 
 Function.prototype.memo = function <A extends any[], R>(
