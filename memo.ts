940bc68aae238be81804bc5c345f7e511ea63722
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-06-07 10:42:02 +0900

    feat(memo): memo
    add interface of cache

--- memo.ts
+++ memo.ts
@@ -1,24 +1,44 @@
 // Copyright 2023-latest Tomoki Miyauchi. All rights reserved. MIT license.
 // This module is browser compatible.
 
-import { compositeKey } from "./composite_key.ts";
+// deno-lint-ignore-file ban-types
 
-export function memo<T, Args extends readonly unknown[], R>(
-  this: T,
-  fn: (...args: Args) => R,
-): (...args: Args) => R {
-  // deno-lint-ignore ban-types
-  const cache = new WeakMap<object, R>();
+import { createCompositeKey, Node } from "./composite_key.ts";
+
+export function memo<T, Args extends unknown[], R>(
+  fn: (this: T, ...args: Args) => R,
+  cache: CacheMap<object, R> = new WeakMap<object, R>(),
+): (this: T, ...args: Args) => R {
+  const node = new Node();
+  const compositeKey = createCompositeKey(node);
 
   return function memoized(this: T, ...args: Args): R {
     const key = compositeKey(this, new.target, ...args);
 
     if (cache.has(key)) return cache.get(key)!;
 
-    const value = fn(...args);
+    const value = fn.apply(this, args);
 
     cache.set(key, value);
 
     return value;
   };
+}
+
+export interface CacheMap<K, V> {
+  /**
+   * @returns a specified element.
+   */
+  get(key: K): V | undefined;
+
+  /**
+   * @returns a boolean indicating whether an element with the specified key exists or not.
+   */
+  has(key: K): boolean;
+
+  /**
+   * Adds a new element with a specified key and value.
+   * @param key Must be an object.
+   */
+  set(key: K, value: V): void;
 }
