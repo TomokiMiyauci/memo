d2a4b85624ad26424f74d2ada6009c67c2d3f19d
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-06-17 17:25:36 +0900

    docs(mod): add module JSDoc tag

--- mod.ts
+++ mod.ts
@@ -4,6 +4,7 @@
 /** This module is memoization tools, ponyfill for TC39 [proposal-function-memo](https://github.com/tc39/proposal-function-memo).
  *
  * You can find [polyfill](./polyfill.ts).
+ * @module
  */
 
 export { type MapLike, memo } from "./memo.ts";
