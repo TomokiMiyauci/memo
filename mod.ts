43cb671fba766bca61c86811d7976bacdc014bee
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-06-17 16:41:28 +0900

    docs(mod): add module description

--- mod.ts
+++ mod.ts
@@ -1,4 +1,9 @@
 // Copyright 2023-latest Tomoki Miyauchi. All rights reserved. MIT license.
 // This module is browser compatible.
 
+/** This module is memoization tools, ponyfill for TC39 [proposal-function-memo](https://github.com/tc39/proposal-function-memo).
+ *
+ * You can find [polyfill](./polyfill.ts).
+ */
+
 export { type MapLike, memo } from "./memo.ts";
